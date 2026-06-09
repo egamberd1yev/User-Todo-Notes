7bba4f1615991d15356aedc071a005ee86c825d1
egamberd1yev <jorabekegamberdiyev12@gmail.com>
2026-06-09 19:17:35 +0500

    data-sourse fixed

--- backend/src/config/data-source.js
+++ backend/src/config/data-source.js
@@ -5,11 +5,28 @@ import { TodoEntity } from "../models/todo.entity.js";
 import { UserEntity } from "../models/user.entity.js";
 dotenv.config();
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  synchronize: true,
-  logging: false,
-  entities: [TodoEntity, UserEntity],
-});
+const isProduction = !!process.env.DATABASE_URL;
+
+export const AppDataSource = new DataSource(
+  isProduction
+    ? {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+        synchronize: true, // Production-da buni false qilish tavsiya etiladi
+        logging: false,
+        entities: [TodoEntity, UserEntity],
+      }
+    : {
+        type: "postgres",
+        host: process.env.DB_HOST || "localhost",
+        port: Number(process.env.DB_PORT || 5432),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: false,
+        synchronize: true,
+        logging: true,
+        entities: [TodoEntity, UserEntity],
+      }
+);
